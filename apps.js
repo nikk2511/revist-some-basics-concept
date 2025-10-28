2bb9f652094fe9a3799cf2ca12107e30834cdab7
Nikhil <nikhil95696@gmail.com>
2025-10-28 13:03:15 +0000

    git commit

--- apps.js
+++ apps.js
@@ -1,9 +1,74 @@
-// function sayMyName(finalName) {
-//     console.log(finalName);
+// function solve(number) {
+//     return function(number) {
+//         return number*number;
+//     }
+// }
+// let ans = solve(5);
+// let finalAnswer =ans(10);
+// console.log(finalAnswer);
+
+// const arr = [
+//     function(a,b) {
+//         return a + b;
+//     },
+//     function(a,b) {
+//         return a - b;
+//     },
+//     function(a,b) {
+//         return a * b;
+//     }
+// ];
+
+// let first = arr[2];
+// let ans = first(5,10);
+// console.log(ans);
+
+// let obj = {
+//     age: 25,
+//     wt:82,
+//     hight:180,
+//     greet: ()=>{
+//         console.log("hello everyone")
+//     }
+// }
+//  console.log(obj.age);
+//  obj.greet();
+
+// let greet = function() {
+//     console.log("hello everyone, how are you")
+// }
+
+// greet()
+
+
+
+// let greet = function() {
+//     console.log("Greeting of the day")
+// }
+
+// greet();
+
+// function greetMe(greet, fullName) {
+//     console.log("hello", fullName);
+//     greet();
 
 // }
 
+// function greet() {
+//     console.log("greeting for the day");
+// }
+
+// greetMe(greet, "nikhil")
+
 // sayMyName("nikhil")
 
-console.log(age);
-let age = 18;
+// console.log(age);
+
+// var age = 18;
+
+// variable hoasting -> whatever we make using this keyword it will more at the top of the code
+// both var(keyword) and function declearation 
+
+// FUNCTION CALL STACK => LIFO(last in first out)
+
+
