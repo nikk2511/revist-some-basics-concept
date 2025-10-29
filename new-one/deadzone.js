f616f7b2fe5a8467cd34d69029c6103a48f3c6ed
Nikhil <nikhil95696@gmail.com>
2025-10-29 07:40:12 +0000

    git commit

---
+++ new-one/deadzone.js
@@ -0,0 +1,35 @@
+// // Variable Scoping 
+// -> Global
+// ->Function
+// ->Block
+
+// var/let/const
+
+// global scope
+
+// var age =15;
+// console.log(age);
+
+// function sayHello() {
+//     var name = "Earth";
+//     console.log("Hello everyone", name)
+
+// }
+
+// sayHello();
+
+
+
+// 
+
+// console.log(fullName);
+
+// function sayHello() {
+//     const fullName ="Earth";
+//     console.log("hello Everyone", name);
+// }
+
+// console.log(fullName);
+// sayHello();
+
+
