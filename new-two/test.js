4e242889cf7df01ad767dec5626622c3cbd13de1
Nikhil <nikhil95696@gmail.com>
2025-10-30 07:50:13 +0000

    git commit

---
+++ new-two/test.js
@@ -0,0 +1,27 @@
+// console.log("nihkil");
+
+
+const accountId = 124532
+
+let emailId = "nikhil@gamil.com"
+
+var accountPassword = "12345"
+
+accountCity = "varansi"
+
+// accountId = 234 // not allowed in terms of const
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+emailId = 'nikhil@example.com' 
+accountPassword = "456789"
+
+accountCity = "delhi"
+
+console.table([accountId, emailId, accountPassword,accountCity])
+
